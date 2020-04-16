4abbc424cb6f135062a917e7cd7a8b4dde0fcaf9
Shiwei Zhou <252485484@qq.com>
2020-04-16 16:05:17 -0700

    milestone 1

--- src/App.js
+++ src/App.js
@@ -1,56 +1,81 @@
 import React, { Component } from "react";
-import { Button, Form } from 'react-bootstrap';
+import { Nav, Navbar, Button, Form, Modal, Row } from "react-bootstrap";
 import "./App.css";
-import BlockChainItem from './components/BlockChainItem'
-import 'bootstrap/dist/css/bootstrap.min.css';
-const crypto = require('crypto')
+import BlockChainItem from "./components/BlockChainItem";
+import "bootstrap/dist/css/bootstrap.min.css";
+const crypto = require("crypto");
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     let today = new Date();
     this.state = {
-      currentTransaction: '',
+      currentTransaction: "",
       chain: [
         {
           index: 0,
-          timestamp: today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(),
+          timestamp:
+            today.getFullYear() +
+            "-" +
+            (today.getMonth() + 1) +
+            "-" +
+            today.getDate() +
+            " " +
+            today.getHours() +
+            ":" +
+            today.getMinutes() +
+            ":" +
+            today.getSeconds(),
           proof: 1,
-          transaction: 'This is genesis block',
-          previous_hash: '0',
+          transaction: "This is genesis block",
+          previous_hash: "0",
           verified: 1,
-        }
-      ]
-    }
+        },
+      ],
+    };
 
     this.generateBlocks = this.generateBlocks.bind(this);
     this.handleTransacChange = this.handleTransacChange.bind(this);
     this.setVerification = this.setVerification.bind(this);
+    this.updateVerification = this.updateVerification.bind(this);
+    this.reVerification = this.reVerification.bind(this);
   }
 
+  /* 
+  hashing algorithm:
+  SHA256(inverse(previous block hash) + ASCii(index + timestamp + transaction + proof))
+  */
   generateBlocks(prev_block, transaction) {
     let prev_data = String(prev_block.previous_hash);
-    prev_data = Buffer.from(prev_data, 'utf8').toString('hex');
+    prev_data = Buffer.from(prev_data, "utf8").toString("hex");
 
     let today = new Date();
-    let time = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    let cur_data = String(prev_block.index + 1) + String(time)
-      + String(transaction)
+    let time =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate() +
+      " " +
+      today.getHours() +
+      ":" +
+      today.getMinutes() +
+      ":" +
+      today.getSeconds();
+    let cur_data =
+      String(prev_block.index + 1) + String(time) + String(transaction);
     let cur_proof = 0;
     let cur_hash = "";
 
     //let pwd = parseInt(prev_data, 16) - parseInt(cur_data_encode, 16)
     //console.log(String(crypto.createHash('sha256').update(pwd).digest('hex')))
-    //algorithm:
     //
-    while (cur_hash.substring(0, 2) !== '00') {
-      console.log(cur_hash)
+    while (cur_hash.substring(0, 2) !== "00") {
       cur_proof = cur_proof + 1;
-      let cur_data_try = cur_data + String(cur_proof)
+      let cur_data_try = cur_data + String(cur_proof);
       //make previous has reverse to prevent constant 0s in front
-      let pwd = prev_data.split("").reverse().join("") + cur_data_try
-      console.log(pwd)
-      cur_hash = String(crypto.createHash('sha256').update(pwd).digest('hex'));
+      let pwd = prev_data.split("").reverse().join("") + cur_data_try;
+      cur_hash = String(crypto.createHash("sha256").update(pwd).digest("hex"));
     }
 
     let new_block = {
@@ -59,58 +84,200 @@ class App extends Component {
       proof: cur_proof,
       transaction: transaction,
       previous_hash: cur_hash,
-      verified: 1
+      verified: 1,
     };
-    this.setState(state => {
+    this.setState((state) => {
       const chain = state.chain.concat(new_block);
       return {
-        chain
-      }
+        chain,
+      };
     });
   }
 
   handleTransacChange(e) {
-    this.setState({ currentTransaction: e.target.value })
+    this.setState({ currentTransaction: e.target.value });
   }
 
   setVerification(index, veriStatus) {
     if (veriStatus === 0 || veriStatus === -1) {
-      this.setState(state => {
+      this.setState((state) => {
         const chain = state.chain;
         for (let i = index; i < chain.length; i++) {
           chain[i].verified = veriStatus;
         }
         return {
-          chain
-        }
+          chain,
+        };
       });
     }
   }
 
-  componentDidMount() {
-    //let today = new Date();
-    //let time = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    //this.setState({
-    //  chain: update(this.state.chain, { 0: { timestamp: { $set: time } } })
-    //})
+  reVerification(index, transaction, timestamp) {
+    if (index === 0) {
+      return;
+    }
+    let prev_block = this.state.chain[index - 1];
+    let prev_data = String(prev_block.previous_hash);
+    prev_data = Buffer.from(prev_data, "utf8").toString("hex");
+    let cur_data = String(index) + String(timestamp) + String(transaction);
+    let cur_proof = 0;
+    let cur_hash = "";
+    while (cur_hash.substring(0, 2) !== "00") {
+      cur_proof = cur_proof + 1;
+      let cur_data_try = cur_data + String(cur_proof);
+      //make previous has reverse to prevent constant 0s in front
+      let pwd = prev_data.split("").reverse().join("") + cur_data_try;
+      cur_hash = String(crypto.createHash("sha256").update(pwd).digest("hex"));
+    }
+    this.setState((state) => {
+      let chain = state.chain;
+      chain[index].previous_hash = cur_hash;
+      chain[index].proof = cur_proof;
+      chain[index].verified = 1;
+      return {
+        chain,
+      };
+    });
   }
 
+  updateVerification(index, transaction, proof, timestamp) {
+    if (index === 0) {
+      return;
+    }
+    let prev_block = this.state.chain[index - 1];
+    let prev_data = String(prev_block.previous_hash);
+    prev_data = Buffer.from(prev_data, "utf8").toString("hex");
+    let cur_data = String(index) + String(timestamp) + String(transaction);
+    let cur_proof = proof;
+    let cur_hash = "";
+    let cur_data_try = cur_data + String(cur_proof);
+    let pwd = prev_data.split("").reverse().join("") + cur_data_try;
+    cur_hash = String(crypto.createHash("sha256").update(pwd).digest("hex"));
+    this.setState((state) => {
+      let chain = state.chain;
+      chain[index].previous_hash = cur_hash;
+      chain[index].proof = proof;
+      chain[index].transaction = transaction;
+      chain[index].timestamp = timestamp;
+      if (cur_hash.substring(0, 2) === "00") {
+        for (let i = index; i < chain.length; i++) {
+          chain[i].verified = 0;
+        }
+      } else {
+        for (let i = index; i < chain.length; i++) {
+          chain[i].verified = -1;
+        }
+      }
+      return {
+        chain,
+      };
+    });
+  }
+
+  componentDidMount() {}
+
   render() {
-    console.log(this.state);
-    //console.log(this.state.chain.slice(-1)[0])
     return (
       <div className="App">
-        <div className="row">
-          {this.state.chain.map(item => (
-            <BlockChainItem setVerification={this.setVerification} key={item.index} item={item} style={{ marginLeft: '20%', marginRight: '20%' }} />
+        <Navbar
+          style={{
+            paddingLeft: "50px",
+            paddingRight: "50px",
+          }}
+          bg="dark"
+          variant="dark"
+          expand="lg"
+        >
+          <Navbar.Brand style={{ fontWeight: "bold" }}>
+            Mini BlockChain by Shiwei Zhou
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="http://www.terryzsw.com">
+                Back to my home page
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="container">
+          <Modal.Dialog style={{ minWidth: "100%" }}>
+            <Modal.Header>
+              <Modal.Title>Instruction</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              <div className="container">
+                <p>
+                  Click GenerateBlocks to create new blocks with content you
+                  want in 'Next transaction' input box
+                </p>
+                <br />
+                <p>
+                  Change transaction coneten in block to see what will happen
+                </p>
+                <br />
+                <p>
+                  Click 'Re-Hash This Block' to generate new proof for valid
+                  hash. (Valid hash have to start with 00)
+                </p>
+                <br />
+                <p>
+                  Green card means verified block. Yellow card means uncerified
+                  block with valid hash. Red card means unvalid hash or broken
+                  link.
+                </p>
+                <br />
+              </div>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <p>Have Fun</p>
+            </Modal.Footer>
+          </Modal.Dialog>
+        </div>
+        <div
+          className="row"
+          style={{
+            paddingLeft: "50px",
+            paddingRight: "50px",
+          }}
+        >
+          {this.state.chain.map((item) => (
+            <BlockChainItem
+              updateVerification={this.updateVerification}
+              reVerification={this.reVerification}
+              key={item.index}
+              item={item}
+              style={{ marginLeft: "20%", marginRight: "20%" }}
+            />
           ))}
         </div>
         <br />
-        <Form.Group controlId="transactionForm" style={{ marginLeft: '20%', marginRight: '20%' }}>
+        <Form.Group
+          controlId="transactionForm"
+          style={{ marginLeft: "20%", marginRight: "20%" }}
+        >
           <Form.Label>Next transaction</Form.Label>
-          <Form.Control type="text" placeholder="It can be any message" onChange={(e) => this.handleTransacChange(e)} /></Form.Group>
-        <Button variant="primary" onClick={(e) => this.generateBlocks(this.state.chain.slice(-1)[0], this.state.currentTransaction, e)}>GenerateBlocks</Button>
-      </div >
+          <Form.Control
+            type="text"
+            placeholder="It can be any message"
+            onChange={(e) => this.handleTransacChange(e)}
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          onClick={(e) =>
+            this.generateBlocks(
+              this.state.chain.slice(-1)[0],
+              this.state.currentTransaction,
+              e
+            )
+          }
+        >
+          GenerateBlocks
+        </Button>
+      </div>
     );
   }
 }
--- src/components/BlockChainItem.js
+++ src/components/BlockChainItem.js
@@ -1,67 +1,174 @@
-import React, { Component } from 'react'
-import { Button, Card, Form } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Button, Card, Form, ListGroup } from "react-bootstrap";
 
 class BlockChainItem extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { cardBackground: [144, 230, 144] }
-        this.handleChange = this.handleChange.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    const {
+      index,
+      timestamp,
+      proof,
+      transaction,
+      previous_hash,
+    } = this.props.item;
+    this.state = {
+      cardBackground: [144, 230, 144],
+      hash: previous_hash,
+      proof: proof,
+      timestamp: timestamp,
+      transaction: transaction,
+      index: index,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(e) {
-        // this.setState(state => {
-        //     state.cardBackground = [255, 255, 153]
-        // });
-        this.props.setVerification(this.props.item.index, 0)
+  handleChange(e, flag) {
+    let {
+      index,
+      timestamp,
+      proof,
+      transaction,
+      previous_hash,
+    } = this.props.item;
+    switch (flag) {
+      case 0:
+        let new_transaction = e.target.value;
+        this.setState((state) => {
+          state.transaction = new_transaction;
+        });
+        this.props.updateVerification(index, new_transaction, proof, timestamp);
+        break;
+      case 1:
+        let new_proof = e.target.value;
+        this.setState((state) => {
+          state.transaction = new_proof;
+        });
+        this.props.updateVerification(index, transaction, new_proof, timestamp);
+        break;
+      case 2:
+        let new_timestamp = e.target.value;
+        this.setState((state) => {
+          state.transaction = new_timestamp;
+        });
+        this.props.updateVerification(index, transaction, proof, new_timestamp);
+        break;
+      default:
+        break;
     }
+  }
 
-    componentWillReceiveProps(props) {
-        if (this.props.item.verified === 1) {
-            this.setState(state => {
-                state.cardBackground = [144, 230, 144]
-            });
-        }
-        else if (this.props.item.verified === 0) {
-            this.setState(state => {
-                state.cardBackground = [255, 255, 153]
-            });
-        }
-        else if (this.props.item.verified === -1) {
-            this.setState(state => {
-                state.cardBackground = [255, 204, 203]
-            });
-        }
+  componentWillReceiveProps(props) {
+    this.setState((state) => {
+      state.hash = this.props.item.previous_hash;
+    });
+    if (this.props.item.verified === 1) {
+      this.setState((state) => {
+        state.cardBackground = [144, 230, 144];
+      });
+    } else if (this.props.item.verified === 0) {
+      this.setState((state) => {
+        state.cardBackground = [255, 255, 153];
+      });
+    } else if (this.props.item.verified === -1) {
+      this.setState((state) => {
+        state.cardBackground = [255, 204, 203];
+      });
     }
+  }
 
-    render() {
-        console.log(this.props.item)
-        const { index, timestamp, proof, transaction, previous_hash } = this.props.item;
-        return (
-            <div >
-                {/* index: {index} <br />
+  componentDidUpdate(prevProps) {
+    if (this.props.item !== prevProps.item) {
+      this.setState((state) => {
+        state.hash = this.props.item.previous_hash;
+      });
+    }
+  }
+
+  render() {
+    console.log(this.state);
+    const {
+      index,
+      timestamp,
+      proof,
+      transaction,
+      previous_hash,
+    } = this.props.item;
+    return (
+      <div>
+        {/* index: {index} <br />
                 hash: {previous_hash} <br />
                 transaction: {transaction} <br />
                 proof: {proof} <br />
                 timestamp: {timestamp} <br /> */}
-                <Card style={{ width: '18rem', background: 'rgb(' + this.state.cardBackground + ')' }} >
-                    <Card.Body>
-                        <Card.Title>Block Index: {index}</Card.Title>
-                        <Card.Text>
-                            <Form.Group onInput={(e) => this.handleChange(e)} >
-                                transaction: <Form.Control size="sm" type="text" defaultValue={transaction} placeholder="transaction" />
-                                proof: <Form.Control size="sm" type="number" defaultValue={proof} placeholder="proof" />
-                                timestamp: <Form.Control size="sm" type="text" defaultValue={timestamp} placeholder="timestamp" />
-                                hash: <Form.Control size="sm" type="text" defaultValue={previous_hash} placeholder="hash" />
-                            </Form.Group>
-                        </Card.Text>
-                        <Button variant="primary" style={{ marginBottom: '5%' }}>Verify This Block</Button>
-                        <Button variant="primary">Re-Hash This Block</Button>
-                    </Card.Body>
-                </Card>
-            </div>
-
-        )
-    }
+        <Card
+          style={{
+            width: "18rem",
+            background: "rgb(" + this.state.cardBackground + ")",
+            marginLeft: "50px",
+            marginBottom: "50px",
+          }}
+        >
+          <Card.Body>
+            <Card.Title>Block Index: {index}</Card.Title>
+            <Card.Text>
+              <Form.Group>
+                transaction:{" "}
+                <Form.Control
+                  size="sm"
+                  type="text"
+                  defaultValue={transaction}
+                  placeholder="transaction"
+                  onInput={(e) => this.handleChange(e, 0)}
+                />
+                proof:{" "}
+                <Form.Control
+                  size="sm"
+                  type="number"
+                  value={proof}
+                  placeholder="proof"
+                  onInput={(e) => this.handleChange(e, 1)}
+                  readOnly
+                />
+                timestamp:{" "}
+                <Form.Control
+                  size="sm"
+                  type="text"
+                  defaultValue={timestamp}
+                  placeholder="timestamp"
+                  onInput={(e) => this.handleChange(e, 2)}
+                  readOnly
+                />
+                original hash:{" "}
+                <Form.Control
+                  size="sm"
+                  type="text"
+                  defaultValue={this.state.hash}
+                  placeholder="hash"
+                  readOnly
+                />
+                current hash:{" "}
+                <ListGroup>
+                  <ListGroup.Item>{this.state.hash}</ListGroup.Item>
+                </ListGroup>
+              </Form.Group>
+            </Card.Text>
+            <Button
+              variant="primary"
+              onClick={() =>
+                this.props.reVerification(
+                  this.state.index,
+                  this.state.transaction,
+                  this.state.timestamp
+                )
+              }
+            >
+              Re-Hash This Block
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
 
-export default BlockChainItem
+export default BlockChainItem;
