dc9441d40511c065a2c89ce0d455781443aa6331
Shiwei Zhou <swchou435@gmail.com>
2025-01-07 21:22:57 -0800

    Updates

---
+++ precache-manifest.c43a99c2680d8df3894bff3ab52d1ad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9f647c223b2d7555be025ae00bf5f2",
+    "url": "/react-mini-blockchain/index.html"
+  },
+  {
+    "revision": "245dafd70a7d5c826232",
+    "url": "/react-mini-blockchain/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1faadb9d6563bba61593",
+    "url": "/react-mini-blockchain/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "245dafd70a7d5c826232",
+    "url": "/react-mini-blockchain/static/js/2.b3534438.chunk.js"
+  },
+  {
+    "revision": "5e819de06af026d7499024f710cfdda3",
+    "url": "/react-mini-blockchain/static/js/2.b3534438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1faadb9d6563bba61593",
+    "url": "/react-mini-blockchain/static/js/main.d962582f.chunk.js"
+  },
+  {
+    "revision": "282dae68e95f0b2d1cb5",
+    "url": "/react-mini-blockchain/static/js/runtime-main.5c75f6b5.js"
+  }
+]);
